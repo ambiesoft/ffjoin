c88b3ea3268c834faabca8113f556b92944e42fb
svnuser <svnuser@5ada3c48-002d-0410-850d-99865cc6fd0f>
2014-01-13 08:46:11 +0000

    start
    git-svn-id: svn://192.168.0.99/ffjoin@4696 5ada3c48-002d-0410-850d-99865cc6fd0f

---
+++ ffjoin/FormMain.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ffjoin
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvMain = new System.Windows.Forms.ListView();
+            this.chFile = new System.Windows.Forms.ColumnHeader();
+            this.btnJoin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lvMain
+            // 
+            this.lvMain.AllowDrop = true;
+            this.lvMain.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chFile});
+            this.lvMain.FullRowSelect = true;
+            this.lvMain.Location = new System.Drawing.Point(12, 12);
+            this.lvMain.MultiSelect = false;
+            this.lvMain.Name = "lvMain";
+            this.lvMain.Size = new System.Drawing.Size(557, 173);
+            this.lvMain.TabIndex = 0;
+            this.lvMain.UseCompatibleStateImageBehavior = false;
+            this.lvMain.View = System.Windows.Forms.View.Details;
+            this.lvMain.DragDrop += new System.Windows.Forms.DragEventHandler(this.lvMain_DragDrop);
+            this.lvMain.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.lvMain_ColumnClick);
+            this.lvMain.DragEnter += new System.Windows.Forms.DragEventHandler(this.lvMain_DragEnter);
+            this.lvMain.DragLeave += new System.EventHandler(this.lvMain_DragLeave);
+            this.lvMain.DragOver += new System.Windows.Forms.DragEventHandler(this.lvMain_DragOver);
+            // 
+            // chFile
+            // 
+            this.chFile.Text = "File";
+            this.chFile.Width = 519;
+            // 
+            // btnJoin
+            // 
+            this.btnJoin.Location = new System.Drawing.Point(12, 191);
+            this.btnJoin.Name = "btnJoin";
+            this.btnJoin.Size = new System.Drawing.Size(75, 23);
+            this.btnJoin.TabIndex = 1;
+            this.btnJoin.Text = "&Join";
+            this.btnJoin.UseVisualStyleBackColor = true;
+            this.btnJoin.Click += new System.EventHandler(this.btnJoin_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 273);
+            this.Controls.Add(this.btnJoin);
+            this.Controls.Add(this.lvMain);
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvMain;
+        private System.Windows.Forms.ColumnHeader chFile;
+        private System.Windows.Forms.Button btnJoin;
+    }
+}
+
---
+++ ffjoin/FormMain.cs
@@ -0,0 +1,180 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace ffjoin
+{
+    public class ListViewItemComparer : System.Collections.IComparer
+    {
+        private int _column;
+
+        /// <summary>
+        /// ListViewItemComparerクラスのコンストラクタ
+        /// </summary>
+        /// <param name="col">並び替える列番号</param>
+        public ListViewItemComparer(int col)
+        {
+            _column = col;
+        }
+
+        //xがyより小さいときはマイナスの数、大きいときはプラスの数、
+        //同じときは0を返す
+        public int Compare(object x, object y)
+        {
+            //ListViewItemの取得
+            ListViewItem itemx = (ListViewItem)x;
+            ListViewItem itemy = (ListViewItem)y;
+
+            //xとyを文字列として比較する
+            return string.Compare(itemx.SubItems[_column].Text,
+                itemy.SubItems[_column].Text);
+        }
+    }
+
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void lvMain_DragDrop(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] fileName = (string[])e.Data.GetData(DataFormats.FileDrop, true);
+                foreach (string s in fileName)
+                {
+                    lvMain.Items.Add(s);
+                }
+            }
+        }
+
+        private void lvMain_DragEnter(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void lvMain_DragLeave(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void lvMain_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void btnJoin_Click(object sender, EventArgs e)
+        {
+            string ext=null;
+            foreach (ListViewItem item in lvMain.Items)
+            {
+                string file = item.Text;
+                FileInfo fi = new FileInfo(file);
+                if (ext == null)
+                    ext = fi.Extension;
+                else
+                {
+                    if (string.Compare(ext, fi.Extension, true) != 0)
+                    {
+                        MessageBox.Show("ext");
+                        return;
+                    }
+                }
+            }
+
+            string outfilename = null;
+            foreach (ListViewItem item in lvMain.Items)
+            {
+                string file = item.Text;
+                FileInfo fi = new FileInfo(file);
+                if (outfilename == null)
+                    outfilename = fi.Name;
+                else
+                {
+                    int isame = 0;
+                    try
+                    {
+                        for (int i = 0; i < fi.Name.Length; ++i)
+                        {
+                            if (fi.Name[i] == outfilename[i])
+                                isame = i+1;
+                            else
+                                break;
+                        }
+                    }
+                    catch (Exception) { }
+
+                    outfilename = outfilename.Substring(0, isame);
+                }
+            }
+            string tempfile = Path.GetTempFileName();
+            using (TextWriter writer = File.CreateText(tempfile))
+            {
+                foreach (ListViewItem item in lvMain.Items)
+                {
+                    writer.Write(@"file '");
+                    writer.Write(item.Text);
+                    writer.Write(@"'");
+                    writer.WriteLine();
+                }
+            }
+
+            string extwithout=ext.TrimStart('.');
+            string outfile=null;
+            using(SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.FileName = outfilename;
+                string filter = extwithout + "File ";
+                filter += "(*.";
+                filter += extwithout;
+                filter += ")|*.";
+                filter += extwithout;
+                filter += "|All File(*.*)|*.*";
+                sfd.Filter=filter;
+                if(DialogResult.OK != sfd.ShowDialog(this))
+                    return;
+
+                outfile = sfd.FileName;
+            }
+
+            FileInfo fithis = new FileInfo(Application.ExecutablePath);
+            string ffmpeg = fithis.Directory + "\\ffmpeg.exe";
+            string argument = "-f concat -i \"";
+
+            argument += tempfile;
+            argument += "\"";
+            argument += " -c copy \"";
+            argument += outfile;
+            argument += "\"";
+            
+
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = ffmpeg;
+            psi.Arguments = argument;
+            psi.RedirectStandardOutput = false;
+            psi.RedirectStandardError = false;
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = false;
+
+            Process p = Process.Start(psi);
+            p.WaitForExit();
+            File.Delete(tempfile);
+        }
+
+        private void lvMain_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            lvMain.ListViewItemSorter = new ListViewItemComparer(e.Column);
+
+        }
+    }
+}
